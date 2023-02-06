9593538383d027569d0160fb68e5949f8073db1f
Abraham-Torres <abraham_torres81@outlook.com>
2023-02-06 12:02:24 -0600

    bugs de password resueltos

--- funciones/Perfil/funciones_perfil.py
+++ funciones/Perfil/funciones_perfil.py
@@ -7,17 +7,20 @@ def perfil():
     if session["ROL"] == "administrador":
         titulo ="Informacion "+str(session["USER"])
         Administradores = BD['admin']
-        Admin = Administradores.find_one({'email':session["USER"]})#REVISAR PORQUE NO OBTIENE EL CAMPO EMAIL (MUESTRA TODO LO QUE TIENE USER)
-        print(Admin)
+        Admin = Administradores.find_one({'email':session["USER"]})
         return render_template('PERFIL/perfil.html', titulo=titulo, admin=Admin)
 
-#CHECAR ESTO xd
+
 def actualizarPassword(key,campo):
     if session ["ROL"] == "administrador":
-        Administradores = BD["admin"]
-        dato = request.form["password"]
-        if dato:
-            print(dato)
-            Administradores.update_one({'correo':key}, {'$set':{campo:dato}}) 
-        return perfil()                   
+        try:
+            Administradores = BD['admin']
+            dato = request.form['password']
+            if dato:
+                Administradores.update_one({'email':key}, {'$set':{campo:dato}}) 
+                flash("CONTRASEÑA ACTUALIZADA CORRECTAMENTE")
+                return perfil()
+        except Exception as e:
+            flash("ERROR FALLO LA ACTUALIZACION DE CONTRASEÑA: "+str(e))        
+            return perfil()                   
     
